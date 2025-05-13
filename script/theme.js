70a7668b8a7ee5f146026af20799d856c2eb2f33
Ashadul <ashadulislam691@gmail.com>
2025-05-13 16:52:28 +0600

    add color

--- script/theme.js
+++ script/theme.js
@@ -1,6 +1,6 @@
 const button = document.getElementById("change-theme");
 const body = document.body;
-const colors = ["#FCE4EC", "#FAF3E0", "#E0F7FA", "#F4F7FF"];
+const colors = ["#0A2647" ,"#FCE4EC", "#FAF3E0", "#E0F7FA", "#F4F7FF" ,"#1C1C1E"];
 let i = 0; 
 button.addEventListener("click", function() {
     if (i < colors.length - 1) { i++; } 
